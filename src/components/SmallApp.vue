b2534b73b08fbdec98a5a3d123cd3b706b361375
Rajashnu <rajashnushakya@gmail.com>
2024-11-18 19:43:59 +0545

    setting up the project

---
+++ src/components/SmallApp.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <!-- Bind the click event to increment the count -->
+  <button @click="count++">Increment Count: {{ count }}</button>
+</template>
